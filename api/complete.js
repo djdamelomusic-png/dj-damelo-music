0f3c698cb038ab272cfa5d28a5fc777ea97e2633
DJ Damelo <dj.damelo.music@gmail.com>
2026-07-12 10:19:11 +0000

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,3 +1,4 @@
-export default async function handler(req, res) {
-  res.status(200).json({ success: true });
+export default function handler(req, res) {
+  // On confirme que le paiement est fini
+  res.status(200).json({ status: "completed" });
 }
