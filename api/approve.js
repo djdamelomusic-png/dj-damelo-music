8e3b295085b6835d17d3a9318550651644e5ddf0
DJ Damelo <dj.damelo.music@gmail.com>
2026-07-12 15:20:54 +0000

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,3 +1,5 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
