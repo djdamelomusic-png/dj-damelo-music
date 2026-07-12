82d0fa902242e782f9556df7388f9baafc50fbd4
DJ Damelo <dj.damelo.music@gmail.com>
2026-07-12 10:39:57 +0000

    Update complete.js

--- api/complete.js
+++ api/complete.js
@@ -1,8 +1,6 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const { paymentId, txid } = req.body;
 
-  // Ici tu pourrais vérifier le paiement sur l'API Pi
-  // Pour l'instant on fait simple
   console.log("Paiement complété:", paymentId, txid);
   
   res.status(200).json({ status: "success" });
